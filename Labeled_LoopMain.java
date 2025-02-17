78c076d70334f24b97437e475ddc97b919624107
Avdhut Madhavi <71624737+KILLERyBEE@users.noreply.github.com>
2025-02-17 20:27:19 +0530

    Labeled Loop

---
+++ Labeled_LoopMain.java
@@ -0,0 +1,8 @@
+import Labled_loop.*;
+public class Labeled_LoopMain {
+    public static void main(String S[])
+    {
+        Loop ob = new Loop();
+        ob.Labled_Loop();
+    }
+}
---
+++ Labled_loop/Loop.java
@@ -0,0 +1,23 @@
+package Labled_loop;
+
+public class Loop 
+{
+    public void Labled_Loop()
+    {
+        Loop1:
+        for(int i=1;i<10;i++)
+        {
+            Loop2:
+            for(int j=2;j<=20;j++)
+            {
+                System.out.println(i + " * " + j + " = " + (i*j));
+                if(i==5)
+                {
+                    break Loop1;
+            
+                }
+            }
+        }
+        
+    }
+}
